baa8594fd7a63c91cd0e181e2ea4df59120d7baa
Austin Ross <mackaustinross@gmail.com>
2019-10-02 09:43:45 -0500

    Initial Commit

---
+++ A3_P1_Mack_Ross/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  A3_P1_Mack_Ross
+//
+//  Created by Austin Ross on 10/2/19.
+//  Copyright © 2019 Mack Ross. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
